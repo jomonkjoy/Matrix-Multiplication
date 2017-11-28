fda78b74a69d1548c6799b721b6adcff51cc8714
Jomon K Joy <jomonkurianjoy@gmail.com>
2017-11-28 13:55:46 +0000

    byte_write generate statement fix

--- byte_write_spram.sv
+++ byte_write_spram.sv
@@ -14,34 +14,21 @@ module byte_write_spram #(
   localparam ADDR_LSB = $clog2(DATA_WIDTH/8);
   localparam ADDR_MSB = $clog2(ADDR_DEPTH)+ADDR_LSB;
   
-  logic [7:0][DATA_WIDTH/8-1:0] mem [ADDR_DEPTH];
+  logic [DATA_WIDTH/8-1:0][7:0] mem [ADDR_DEPTH];
   
-  logic mem_if_write_r;
-  logic [ADDR_WIDTH-1:0] mem_if_address_r;
-  logic [DATA_WIDTH-1:0] mem_if_write_data_r;
-  logic [DATA_WIDTH/8-1:0] mem_if_write_strb_r;
-  
+  genvar i;
+  generate for (i=0; i<DATA_WIDTH/8; i++) begin : write_byte
   always_ff @(posedge clk) begin
-    mem_if_write_r <= mem_if_write;
-    mem_if_address_r <= mem_if_address;
-    mem_if_write_data_r <= mem_if_write_data;
-    mem_if_write_strb_r <= mem_if_write_strb;
-  end
-  integer i;
-  always_ff @(posedge clk) begin
-    if (mem_if_write_r) begin
-      for (i=0; i<DATA_WIDTH/8; i++) begin : strb
-        if (mem_if_write_strb_r[i]) begin
-          mem[mem_if_address_r[ADDR_MSB:ADDR_LSB]][i] <= mem_if_write_data_r[8*i+:8];
-        end else begin
-          mem[mem_if_address_r[ADDR_MSB:ADDR_LSB]][i] <= mem_if_read_data[8*i+:8];
-        end
+    if (mem_if_write) begin
+      if (mem_if_write_strb[i]) begin
+        mem[mem_if_address[ADDR_MSB-1:ADDR_LSB]][i] <= mem_if_write_data[8*i+:8];
       end
     end
   end
+  end endgenerate
   
   always_ff @(posedge clk) begin
-    mem_if_read_data <= mem[mem_if_address[ADDR_MSB:ADDR_LSB]];
+    mem_if_read_data <= mem[mem_if_address[ADDR_MSB-1:ADDR_LSB]];
   end
   
 endmodule
