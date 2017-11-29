878881786c258331ef0c75cc3b39727a24dae724
Jomon K Joy <jomonkurianjoy@gmail.com>
2017-11-29 01:54:51 +0000

    Update asymmertic_ram.sv

--- asymmertic_ram.sv
+++ asymmertic_ram.sv
@@ -11,7 +11,29 @@ module asymmertic_wr_ram #(
   input  logic [DATA_WIDTH/8-1:0] mem_if_write_strb,
   output logic [DATA_RATIO][DATA_WIDTH-1:0] mem_if_read_data
 );
-
+  
+  localparam ADDR_LSB = $clog2(DATA_WIDTH/8);
+  localparam ADDR_MSB = $clog2((DATA_RATIO*DATA_WIDTH)/8);
+  
+  logic [DATA_RATIO][DATA_WIDTH-1:0] int_if_write_data;
+  logic [DATA_RATIO][DATA_WIDTH/8-1:0] int_if_write_strb;
+  
+  assign int_if_write_data = {DATA_RATIO{mem_if_write_data}};
+  assign int_if_write_strb = mem_if_write_strb << mem_if_address[ADDR_MSB-1:ADDR_LSB];
+  
+  byte_write_spram #(
+    .ADDR_DEPTH(ADDR_DEPTH),
+    .ADDR_WIDTH(ADDR_WIDTH),
+    .DATA_WIDTH(DATA_WIDTH*DATA_RATIO)
+  ) byte_write_spram_inst (
+    .clk(clk),
+    .mem_if_write(mem_if_write),
+    .mem_if_address(mem_if_address),
+    .mem_if_write_data(int_if_write_data),
+    .mem_if_write_strb(int_if_write_strb),
+    .mem_if_read_data(mem_if_read_data)
+  );
+  
 endmodule
 
 module asymmertic_rd_ram #(
@@ -27,5 +49,30 @@ module asymmertic_rd_ram #(
   input  logic [DATA_RATIO][DATA_WIDTH/8-1:0] mem_if_write_strb,
   output logic [DATA_WIDTH-1:0] mem_if_read_data
 );
-
+  
+  localparam ADDR_LSB = $clog2(DATA_WIDTH/8);
+  localparam ADDR_MSB = $clog2((DATA_RATIO*DATA_WIDTH)/8);
+  
+  logic [ADDR_WIDTH-1:0] mem_if_address_r;
+  logic [DATA_RATIO][DATA_WIDTH-1:0] int_if_read_data;
+  
+  always_ff @(posedge clk) begin
+    mem_if_address_r <= mem_if_address;
+  end
+  
+  assign mem_if_read_data = int_if_read_data[mem_if_address_r[ADDR_MSB-1:ADDR_LSB]];
+  
+  byte_write_spram #(
+    .ADDR_DEPTH(ADDR_DEPTH),
+    .ADDR_WIDTH(ADDR_WIDTH),
+    .DATA_WIDTH(DATA_WIDTH*DATA_RATIO)
+  ) byte_write_spram_inst (
+    .clk(clk),
+    .mem_if_write(mem_if_write),
+    .mem_if_address(mem_if_address),
+    .mem_if_write_data(mem_if_write_data),
+    .mem_if_write_strb(mem_if_write_strb),
+    .mem_if_read_data(int_if_read_data)
+  );
+  
 endmodule
